e61fe066466f47efab37e8e50eece9fc1f2baf59
Russell Bush <bush@krtkl.com>
2018-09-12 11:07:50 -0700

    Added auto config in HDMI enable

--- projects/hdmi_en.sh
+++ projects/hdmi_en.sh
@@ -9,3 +9,8 @@ echo out > /sys/class/gpio/gpio959/direction
 echo 1 > /sys/class/gpio/gpio958/value
 echo 1 > /sys/class/gpio/gpio959/value
 
+sleep 2
+
+build/hdmi-config -m 1920x1080
+build/uio-vtc -m 1920x1080 12
+build/vid-tpg-config -w 1920 -h 1080 -b 0 13
